c128748c452c6b04e7d32313bdad0e0cddb493bf
Medad Rufus Newman <medad@medadnewman.co.uk>
2026-01-24 01:22:28 +0000

    Revert "Handle routing"
    This reverts commit 332df77742a320d87f6fea895520ffbd94174563.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,16 +1,3 @@
 from app import app as application
 
 # Passenger (cPanel) will look for `application` callable.
-
-# Middleware to ensure Flask generates URLs with the correct subdirectory prefix
-class SubdirectoryMiddleware:
-    def __init__(self, app, prefix):
-        self.app = app
-        self.prefix = prefix
-
-    def __call__(self, environ, start_response):
-        # Force SCRIPT_NAME to the prefix so url_for() generates correct paths
-        environ['SCRIPT_NAME'] = self.prefix
-        return self.app(environ, start_response)
-
-application.wsgi_app = SubdirectoryMiddleware(application.wsgi_app, '/concordance')
