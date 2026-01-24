3aae098da80b288e348c8c0ef89b603e129a499e
Medad Newman <medad@medadnewman.co.uk>
2026-01-24 00:41:47 +0000

    Don't go off to the right (#5)
    Preview to realign to the left so that it does not go off to the right.
    Issue was:
    Would it be possible that the frame/box that brings up the Bible verse will flip inwards when it comes to the end of the page?
    At the moment some of the box with the text is cut off when I get to the right edge of the pages.

--- add_hyperlinks.py
+++ add_hyperlinks.py
@@ -333,6 +333,11 @@ def main():
         }
 
         .bible-ref-missing:hover::after { opacity: 1; }
+
+        .bible-ref.right-aligned::after, .bible-ref-missing.right-aligned::after {
+            left: auto;
+            right: 0;
+        }
     """
 
     write_html(paragraphs, html_path, style)
