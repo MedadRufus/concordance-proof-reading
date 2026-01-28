4df54b2f20424a35a8abc4834f8fcf7672daca38
Medad Rufus Newman <medad@medadnewman.co.uk>
2026-01-28 22:19:03 +0000

    isort and remove unneeded

--- tests/test_root_matching.py
+++ tests/test_root_matching.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Unit tests for the root word matching functionality using pytest."""
 
-import sys
 import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+import sys
 
 import pytest
+
 from add_hyperlinks import Reference
 
 
